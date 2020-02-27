5bf20dfe3cc02dc63a8f76daf9de206f97fefdaa
Durgesh Tiwari <d.k.tiwari692@gmail.com>
2020-02-27 11:20:13 +0530

    first git commit

---
+++ assets/effect.js
@@ -0,0 +1,328 @@
+// JavaScript Document
+var menu1_chk=1;
+var dcon = "";
+var sname = "";
+var roll = "";
+var time = 0;
+function logoin(){
+	izero();
+	document.getElementById("clogo").style.transition= ".6s";
+	document.getElementById("clogo").style.width= "44vw";
+	document.getElementById("clogo").style.left= "28vw";
+	document.getElementById("clogo").style.height= "3.5vw";
+	document.getElementById("clogo").style.top= "4vw";
+	document.getElementById("clogo").style.boxShadow= "0 3px 4px 1px grey";
+	setTimeout(tranof,1500);
+	setTimeout(logo2in,100);
+	setTimeout(menu1in,100);
+	setTimeout(menu1_expand,800);
+}
+function logo2in(){
+	document.getElementById("logo2").style.transition= ".6s";
+	document.getElementById("logo2").style.top= "11vw";
+	document.getElementById("logo2").style.left= "15vw";
+	document.getElementById("logo2").style.height= "3vw";
+	document.getElementById("logo2").style.width= "70vw";
+}
+function menu1in(){
+	document.getElementById("smenuid").style.transition= ".6s";
+	document.getElementById("smenuid").style.top= "14vw";
+	document.getElementById("smenuid").style.height= "0vw";
+	document.getElementById("smenuid").style.width= "70vw";
+	document.getElementById("smenuid").style.left= "15vw";
+}
+
+function menu1_expand(){
+	document.getElementById("smenuid").style.height= "27vw";
+	document.getElementById("smenuid").style.zIndex= "-1";
+}
+function resultmenu1(){
+	document.getElementById("smenu1id").style.width= "40vw";
+	document.getElementById("smenu1id").style.transition= ".6s";
+	setTimeout(resultmenu2,300);
+	setTimeout(resultheading1,300);
+	btn1ext();
+	setTimeout(tranof,900);
+}
+function resultmenu2(){
+	document.getElementById("smenu1id").style.left= "45vw";
+	document.getElementById("btn1id").innerHTML= "Exams";
+	rmenu();
+	smenu2();
+	btn1srnk();	
+	setTimeout(resultmenu3,300);
+}
+function resultmenu3(){
+	document.getElementById("smenu1id").style.width= "25vw";
+	document.getElementById("smenu1id").style.left= "45vw";
+	document.getElementById("smenu1id").style.boxShadow= "-2px 0 3px 2px rgba(0,0,0,0.28)";
+	document.getElementById("smenu1id").style.transition= ".3s";
+}
+function resultheading1(){
+	document.getElementById("smenu1_1id").style.left= "-32vw";
+	document.getElementById("smenu1_1id").style.transition= ".3s"
+	document.getElementById("smenu1_2id").style.left= "-32vw";
+	document.getElementById("smenu1_2id").style.transition= ".3s";
+	document.getElementById("smenu1_3id").style.transition= ".3s";
+	document.getElementById("smenu1_3id").style.left= "-29vw";
+}
+function btn1ext(){
+	document.getElementById("btn1id").style.width= "29vw";
+	document.getElementById("btn1id").style.transition= ".6s";
+	document.getElementById("btn1id").disabled= true;
+}
+function btn1srnk(){
+	document.getElementById("btn1id").style.width= "14vw";
+	document.getElementById("btn1id").disabled= false;
+}
+function rmenu(){
+	document.getElementById("rmenu1_1").style.left= "0vw";
+	document.getElementById("rmenu1_1").style.transition= ".6s";
+	document.getElementById("rmenu1_2").style.left= "0vw";
+	document.getElementById("rmenu1_2").style.transition= ".6s";
+	document.getElementById("rmenu1_3").style.left= "0vw";
+	document.getElementById("rmenu1_3").style.transition= ".6s";
+}
+function smenu2(){
+	document.getElementById("smenu2id").style.left= "-3vw";
+	document.getElementById("smenu2id").style.transition= ".6s";
+	setTimeout(smenu2_text,200);
+}
+function smenu2_text(){
+	document.getElementById("smenu2_1id").innerHTML= "Authorization Menu";
+	document.getElementById("smenu2_2id").innerHTML= "Username";
+	document.getElementById("smenu2_3id").innerHTML= "Password";
+	document.getElementById("smenu2_4id").innerHTML= "";
+	document.getElementById("b2").innerHTML= "Login";
+	document.getElementById("i2").type= "password";
+	izero();
+}
+function menuchkr(){
+	if(menu1_chk==1){
+		resultmenu1();
+		menu1_chk = 0;
+	} else {
+		menu1_chk = 1;
+		resultmenubck();
+	}
+}
+function resultmenubck() {
+	document.getElementById("smenu1id").style.width= "40vw";
+	document.getElementById("smenu1id").style.left= "30vw";
+	document.getElementById("smenu1id").style.transition= ".6s";
+	document.getElementById("rmenu1_1").style.left= "15vw";
+	document.getElementById("rmenu1_1").style.transition= ".6s";
+	document.getElementById("rmenu1_2").style.left= "15vw";
+	document.getElementById("rmenu1_2").style.transition= ".6s";
+	document.getElementById("rmenu1_3").style.left= "15vw";
+	document.getElementById("rmenu1_3").style.transition= ".6s";
+	btn1ext();
+	setTimeout(resultmenubck1,300);
+	setTimeout(tranof,1100);
+}
+function resultmenubck1(){
+	document.getElementById("smenu1id").style.left= "0vw";
+	document.getElementById("smenu1id").style.transition= ".6s";
+	btn1srnk();
+	document.getElementById("btn1id").innerHTML= "Results";
+	document.getElementById("btn1id").style.right= "5.5vw";
+	setTimeout(resultmenubck2,200);
+	rmenubck1();
+	smenu2bck();
+}
+function resultmenubck2(){
+	document.getElementById("smenu1id").style.width= "25vw";
+	document.getElementById("smenu1id").style.left= "0vw";
+	document.getElementById("btn1id").style.left= "5.5vw";
+	document.getElementById("smenu1id").style.transition= ".6s";
+	document.getElementById("smenu1id").style.boxShadow= "2px 0 3px 2px rgba(0,0,0,0.28)";
+	smenu1bck();
+}
+function rmenubck1(){
+	document.getElementById("rmenu1_1").style.left= "40vw";
+	document.getElementById("rmenu1_2").style.left= "40vw";
+	document.getElementById("rmenu1_3").style.left= "40vw";
+	document.getElementById("rmenu1_1").style.transition= ".3s";
+	document.getElementById("rmenu1_2").style.transition= ".3s";
+	document.getElementById("rmenu1_3").style.transition= ".3s";
+}
+function smenu1bck(){
+	document.getElementById("smenu1_1id").style.left= "0vw";
+	document.getElementById("smenu1_1id").style.transition= ".4s"
+	document.getElementById("smenu1_2id").style.left= "0vw";
+	document.getElementById("smenu1_2id").style.transition= ".4s";
+	document.getElementById("smenu1_3id").style.transition= ".4s";
+	document.getElementById("smenu1_3id").style.left= "0vw";
+}
+function smenu2bck(){
+	document.getElementById("smenu2id").style.left= "25vw";
+	document.getElementById("smenu2id").style.transition= ".6s";
+	setTimeout(smenu2_textbck,200);
+}
+function smenu2_textbck(){
+	document.getElementById("smenu2_1id").innerHTML= "Configure Database";
+	document.getElementById("smenu2_2id").innerHTML= "Database Name";
+	document.getElementById("smenu2_3id").innerHTML= "Table Name";
+	document.getElementById("smenu2_4id").innerHTML= "*New databases must have 50 QUESTIONS with 4 OPTIONS and answer in 5<sup>TH</sup> column";
+	document.getElementById("b2").innerHTML= "Proceed!";
+	izero();
+	document.getElementById("i2").type= "text";
+}
+function tranof(){
+	document.getElementById("clogo").style.transition= "none";
+	document.getElementById("logo2").style.transition= "none";
+	document.getElementById("smenuid").style.transition= "none";
+	document.getElementById("smenu1id").style.transition= "none";
+	document.getElementById("smenu1_1id").style.transition= "none";
+	document.getElementById("smenu1_2id").style.transition= "none";
+	document.getElementById("smenu1_3id").style.transition= "none";
+	document.getElementById("btn1id").style.transition= "none";
+	document.getElementById("rmenu1_1").style.transition= "none";
+	document.getElementById("rmenu1_2").style.transition= "none";
+	document.getElementById("rmenu1_3").style.transition= "none";
+	document.getElementById("smenu2id").style.transition= "none";
+	document.getElementById("smenu2_1id").style.transition= "none";
+	document.getElementById("smenu2_2id").style.transition= "none";
+	document.getElementById("smenu2_3id").style.transition= "none";
+	document.getElementById("smenu2_4id").style.transition= "none";
+	document.getElementById("error1id").style.transition= "none";
+	document.getElementById("war1id").style.transition= "none";
+	document.getElementById("name1").style.transition= "none";
+	document.getElementById("menutxt2").style.transition= "none";
+	document.getElementById("nameshow").style.transition= "none";
+	document.getElementById("img_con1").style.transition= "none";
+}
+function izero(){
+	document.getElementById("i1").value= "";
+	document.getElementById("i2").value= "";
+	document.getElementById("i3").value= "";
+	document.getElementById("i4").value= "";
+	document.getElementById("i5").value= "";
+}
+function con1(){
+	var data1 = document.getElementById("i1").value;
+	var tab1 = document.getElementById("i2").value;
+	var con = new XMLHttpRequest();
+	con.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200){
+			dcon = this.responseText;
+		}
+	};
+	con.open("GET", "assets/contest.php?db=" + data1 + "&tb=" + tab1, true);
+	con.send();
+}
+function dchk(){
+	if(dcon == "yes"){
+		
+	}else{
+		errorlog();
+		setTimeout(errorlogmenubck,100);
+	}
+}
+function menu1chk1(){
+	if(menu1_chk==1){
+		con1();
+		setTimeout(dchk,500);
+	}else{
+		
+	}
+}
+function errorlog(){
+	document.getElementById("error1id").style.transition= ".6s";
+	document.getElementById("error1id").style.display= "block";
+	document.getElementById("error1id").style.top= "0px";
+	document.getElementById("error1id").style.left= "0px";
+	document.getElementById("error1id").style.height= "100vh";
+	document.getElementById("error1id").style.width= "100vw";
+	document.getElementById("error1id").style.zIndex= "2";
+	document.getElementById("error1id").style.overflow= "hidden";
+	document.getElementById("error1id").style.position= "fixed";
+	document.body.style.overflow= "hidden";
+	setTimeout(errorlogmenu,200);
+}
+function errorlogmenu(){
+	document.getElementById("war1id").style.transition= ".6s";
+	document.getElementById("war1id").style.display= "block";
+	document.getElementById("war1id").style.top= "33vh";
+	document.getElementById("war1id").style.left= "28vw";
+	document.getElementById("war1id").style.height= "15vw";
+	document.getElementById("war1id").style.zIndex= "3";
+	document.getElementById("war1id").style.width= "44vw";
+	setTimeout(tranof,800);
+}
+function errorlogbck(){
+	document.getElementById("error1id").style.transition= ".6s";
+	document.getElementById("error1id").style.display= "block";
+	document.getElementById("error1id").style.top= "50vh";
+	document.getElementById("error1id").style.left= "50vw";
+	document.getElementById("error1id").style.height= "0vh";
+	document.getElementById("error1id").style.width= "0vw";
+	document.getElementById("error1id").style.zIndex= "4";
+	document.getElementById("error1id").style.overflow= "hidden";
+	document.getElementById("error1id").style.position= "fixed";
+	document.body.style.overflow= "visible";
+	setTimeout(errorlogmenubck,100);
+}
+function errorlogmenubck(){
+	document.getElementById("war1id").style.transition= ".6s";
+	document.getElementById("war1id").style.display= "block";
+	document.getElementById("war1id").style.top= "50vh";
+	document.getElementById("war1id").style.left= "50vw";
+	document.getElementById("war1id").style.height= "0vw";
+	document.getElementById("war1id").style.zIndex= "4";
+	document.getElementById("war1id").style.width= "0vw";
+	setTimeout(tranof,800);
+}
+function menu1shrnk(){
+	document.getElementById("smenuid").style.height= "0vw";
+	document.getElementById("smenuid").style.zIndex= "-1";
+	document.getElementById("smenuid").style.transition= ".6s";
+	document.getElementById("menutxt1").style.transition= ".6s";
+	document.getElementById("menutxt2").style.transition= ".6s";
+	document.getElementById("menutxt1").style.top= "-3vw";
+	document.getElementById("menutxt2").style.top= "0vw";
+	logoout();
+}
+function logoout(){
+	document.getElementById("clogo").style.transition= ".6s";
+	document.getElementById("clogo").style.backgroundColor= "#BDD6E0";
+	document.getElementById("clogo").style.top= "1vw";
+	document.getElementById("clogo").style.boxShadow= "none";
+	document.getElementById("clogop").style.color= "#BDD6E0";
+	setTimeout(header1,600);
+}
+function header1(){
+	document.getElementById("logo2").style.transition= ".6s";
+	document.getElementById("logo2").style.top= "0vw";
+	document.getElementById("logo2").style.left= "0vw";
+	document.getElementById("logo2").style.height= "3vw";
+	document.getElementById("logo2").style.width= "100vw";
+	document.getElementById("clogo").style.display= "none";
+	document.getElementById("clogop").style.display= "none";
+	document.getElementById("smenuid").style.display= "none";
+	document.getElementById("img_con1").style.paddingLeft= ".5vw";
+	document.getElementById("menutxt2").innerHTML= "Test Menu";
+	document.getElementById("img_con1").style.transition= ".6s";
+	document.getElementById("menutxt2").style.left= "4vw";
+	setTimeout(namein,600);
+}
+function namein(){
+	document.getElementById("name1").style.transition= ".6s";
+	document.getElementById("name1").style.top= "30vh";
+	setTimeout(tranof,600);
+}
+function nameout(){
+	document.getElementById("name1").style.transition= ".6s";
+	document.getElementById("name1").style.top= "-30vw";
+	sname = document.getElementById("i3").value.toUpperCase();
+	roll = document.getElementById("i4").value.toUpperCase();
+	time = document.getElementById("i5").value;
+	nameshow();
+}
+function nameshow(){
+	document.getElementById("nameshow").style.transition= ".6s";
+	document.getElementById("nameshow").style.left= "0vw";
+	document.getElementById("sname").innerHTML= sname;
+	document.getElementById("rnumber").innerHTML= roll;
+	setTimeout(tranof,800);
+}
---
+++ index.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Present Test</title>
+<link href="assets/effect.css" rel="stylesheet" type="text/css">
+<script src="assets/effect.js"></script>
+</head>
+
+<body onLoad="setTimeout(logoin,100);">
+	<div id="error1id" class="error1"></div>
+	<div class="war1" id="war1id">
+		<img src="assets/owlcom.jpg" class="war1_img">
+		<p class="war1_head"> OOPS! Owl Found an ERROR! </p>
+		<p class="war1_subhead">Owl couldn't find the Database You're looking for</p>
+		<ul>Here's a few things you can do to help: - 
+			<li>Make sure servers are running without any issue</li>
+			<li>Make sure Webbrowser have given proper permissions</li>
+			<li>Make sure the database in question exists in database</li>
+			<li>If all else fails call cornade he must know something</li>
+		</ul>
+		<button type="submit" class="btn3" onclick="errorlogbck()">Let's Try Again</button>
+	</div>
+	<div id="clogo" class="compdesc">
+		<p id="clogop">Welcome To The Project Made By BrokenOwlCorporation&reg;</p>
+	</div>
+	<div id="logo2" class="login_container">
+		<img src="assets/my logo2.png" class="img_container" id="img_con1">
+		<p id="menutxt1">Main Menu</p><p id="menutxt2">Success!</p>
+	</div>
+	<div class="smenu" id="smenuid">
+		<div class="smenu1" id="smenu1id">
+			<p class="smenu1_1" id="smenu1_1id">Examination Menu</p>
+			<p class="smenu1_2" id="smenu1_2id">Enter your Custom Database and Table <br> to give exam from that table or leave it <br> Blank to use Default Database.</p>
+			<p class="smenu1_3" id="smenu1_3id">Want to view Results instead?</p>
+			<button type="submit" class="btn1" onClick="menuchkr()" id="btn1id">Results</button>
+			<div class="smenu1_1" id="rmenu1_1">Results Menu</div>
+			<div class="smenu1_2" id="rmenu1_2">Due to privacy and Sensitivity of <br> information you must enter Username <br> and Pasword to continue</div>
+			<div class="smenu1_3" id="rmenu1_3">Want to go back to Exam Menu?</div>
+		</div>
+		<div class="smenu2" id="smenu2id">
+			<p class="smenu2_1" id="smenu2_1id">Configure Database</p>
+			<p class="smenu2_2" id="smenu2_2id">Database Name</p>
+			<input type="text" id="i1" class="inp1" >
+			<p class="smenu2_3" id="smenu2_3id">Table Name</p>
+			<input type="text" id="i2" class="inp2">
+			<button class="btn2" id="b2" onClick="menu1shrnk()">Proceed!</button>
+			<p class="smenu2_4" id="smenu2_4id">*New databases must have 50 QUESTIONS with 4 OPTIONS and answer in 5<sup>TH</sup> column</p>
+		</div>
+		
+	</div>
+	<div id="name1">
+		<p class="name1-1">Name: -</p>
+		<input type="text" id="i3" class="inp3">
+		<p class="name1-2">Roll Number: -</p>
+		<input type="text" id="i4" class="inp4">
+		<p class="name1-3">Time: -(in Minutes)</p>
+		<input type="text" id="i5" class="inp5">
+		<p class="name1-4">*Leave Blank for Default 25 Min.</p>
+		<button type="submit" class="btn4" onClick="nameout()">Submit</button>
+	</div>
+	<div id="nameshow">
+		<p class="nameshow1">Name: - </p>
+		<p class="nameshow2">Roll Number: -</p>
+		<p id="sname"></p>
+		<p id="rnumber"></p>
+	</div>
+</body>
+</html>
